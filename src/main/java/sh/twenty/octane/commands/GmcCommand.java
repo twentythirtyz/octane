31ca04a092eec1f1bd426a2d93c48f32747def2f
twenty1989 <big23@ntrotech.com>
2026-05-12 19:07:11 +0300

    smoking runts to the face

--- src/main/java/sh/twenty/octane/commands/GmcCommand.java
+++ src/main/java/sh/twenty/octane/commands/GmcCommand.java
@@ -6,33 +6,47 @@ import sh.twenty.octane.utils.ChatUtil;
 import sh.twenty.octane.Main;
 
 public class GmcCommand extends PlayerCommand {
-    public GmcCommand(Main plugin) {super(plugin, "octane.gmc"); }
+
+    public GmcCommand(Main plugin) {
+        super(plugin, "octane.gmc");
+    }
 
     @Override
     public boolean execute(Player player, String[] args){
-        if (args.length >= 1){
+        if (args.length >= 1) {
 
-            if(!player.hasPermission("octane.gmc.others")){
-                String rawMessage = plugin.getConfigManager().getMessages().getString("no-permission", "&cNo permission!");
-                player.sendMessage(ChatUtil.color(rawMessage));
-                return false;
+            if (!player.hasPermission("octane.gmc.others")) {
+                String noPerm = plugin.getConfigManager().getMessages().getString("no-permission", "&cNo permission!");
+                player.sendMessage(ChatUtil.color(noPerm));
+                return true;
             }
 
             Player target = plugin.getServer().getPlayerExact(args[0]);
 
-            if (target == null){
-                sendPrefixedMessage(player, "&cCould not find player: " + args[0]);
+            if (target == null) {
+                String notFound = plugin.getConfigManager().getMessages().getString("player-not-found", "&cCould not find player: %player%");
+                sendPrefixedMessage(player, notFound.replace("%player%", args[0]));
                 return true;
             }
 
             target.setGameMode(GameMode.CREATIVE);
 
-            sendPrefixedMessage(player, "&7" + target.getName() + " had his gamemode set to Creative.");
-            sendPrefixedMessage(target, "&7Your gamemode was set to Creative by " + player.getName() + ".");
+
+            String successOther = plugin.getConfigManager().getMessages().getString("gmc-success-other", "&7%player%'s gamemode was set to Creative.");
+            sendPrefixedMessage(player, successOther.replace("%player%", target.getName()));
+
+            String successTarget = plugin.getConfigManager().getMessages().getString("gmc-success-target", "&7Your gamemode was set to Creative by %sender%.");
+            sendPrefixedMessage(target, successTarget.replace("%sender%", player.getName()));
+
             return true;
         }
+
         player.setGameMode(GameMode.CREATIVE);
-        sendPrefixedMessage(player, "&7Your gamemode was set to Creative.");
+
+
+        String successSelf = plugin.getConfigManager().getMessages().getString("gmc-success-self", "&7Your gamemode was set to Creative.");
+        sendPrefixedMessage(player, successSelf);
+
         return true;
     }
 }
--- src/main/java/sh/twenty/octane/commands/GmsCommand.java
+++ src/main/java/sh/twenty/octane/commands/GmsCommand.java
@@ -13,26 +13,33 @@ public class GmsCommand extends PlayerCommand{
     public boolean execute(Player player, String[] args){
         if (args.length >= 1){
             if(!player.hasPermission("octane.gms.others")){
-                String rawMessage = plugin.getConfigManager().getMessages().getString("no-permission", "&cNo permission!");
-                player.sendMessage(ChatUtil.color(rawMessage));
+                String noPerm = plugin.getConfigManager().getMessages().getString("no-permission", "&cNo permission!");
+                player.sendMessage(ChatUtil.color(noPerm));
                 return false;
             }
 
             Player target = plugin.getServer().getPlayerExact(args[0]);
 
             if(target == null){
-                sendPrefixedMessage(player, "&cCould not find the player: " + args[0]);
+                String notFound = plugin.getConfigManager().getMessages().getString("player-not-found", "&cCould not find player: %player%");
+                sendPrefixedMessage(player, notFound.replace("%player%", args[0]));
                 return true;
             }
 
             target.setGameMode(GameMode.SURVIVAL);
 
-            sendPrefixedMessage(player, "&7" + target.getName() + " had his gamemode set to Survival.");
-            sendPrefixedMessage(target, "&7Your gamemode was set to Survival by " + player.getName() + ".");
+            String successOther = plugin.getConfigManager().getMessages().getString("gms-success-other", "&7%player%'s gamemode was set to Survival.");
+            sendPrefixedMessage(player, successOther.replace("%player%", target.getName()));
+
+            String successTarget = plugin.getConfigManager().getMessages().getString("gms-success-target", "&7%player%'s gamemode was set to Survival.");
+            sendPrefixedMessage(player, successTarget.replace("%player%", target.getName()));
             return true;
         }
         player.setGameMode(GameMode.SURVIVAL);
-        sendPrefixedMessage(player, "&7Your gamemode was set to Survival.");
+
+
+        String successSelf = plugin.getConfigManager().getMessages().getString("gms-success-self", "&7Your gamemode was set to Survival");
+        sendPrefixedMessage(player, successSelf);
         return true;
     }
 }
